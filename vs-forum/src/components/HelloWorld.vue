ac70f2c85a66d1fb38b224dc5a2e75303dabc45d
Peter <pesegal@gmail.com>
2019-06-24 22:04:21 -0700

    feat: fix user name

--- vs-forum/src/components/HelloWorld.vue
+++ vs-forum/src/components/HelloWorld.vue
@@ -4,7 +4,7 @@
   <div v-for="thread in threads">
     <h2>{{thread.title}}</h2>
     <div v-for="postId in thread.posts">
-      <p>{{users[posts[postId]].userId}}</p>
+      <p>{{users[posts[postId].userId].name}}</p>
       <p>{{posts[postId].text}}</p>
     </div>
   </div>
