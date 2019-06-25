f3165d1962911831bc78451a2b71bbbeecfe2e29
Peter <pesegal@gmail.com>
2019-06-24 22:24:05 -0700

    feat: add styling

--- vs-forum/src/App.vue
+++ vs-forum/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <div class="container">
+      <router-view/>
+    </div>
   </div>
 </template>
 
--- vs-forum/src/components/HelloWorld.vue
+++ vs-forum/src/components/HelloWorld.vue
@@ -1,15 +1,24 @@
 <template>
-<div>
-  <h1>Welcome to the Forum</h1>
-  <div v-for="thread in threads">
-    <h2>{{thread.title}}</h2>
-    <div v-for="postId in thread.posts">
-      <p>{{users[posts[postId].userId].name}}</p>
-      <p>{{posts[postId].text}}</p>
+  <div>
+    <div class="col-large push-top" v-for="thread in threads">
+      <h1>{{thread.title}}</h1>
+      <div class="post-list">
+        <div class="post" v-for="postId in thread.posts">
+          <div class="user-info">
+            <a href="#">{{users[posts[postId].userId].name}}</a>
+            <a href="#">
+              <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt>
+            </a>
+            <p class="desktop-only text-small">104 posts</p>
+          </div>
+          <div class="post-content">
+            <p>{{posts[postId].text}}</p>
+          </div>
+          <div class="post-date text-faded">{{posts[postId].publishedAt}}</div>
+        </div>
+      </div>
     </div>
   </div>
-</div>
-
 </template>
 
 <script>
