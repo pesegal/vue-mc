6b7b9eb2d10523c12aa840b99f01bdecd445faa4
Peter <pesegal@gmail.com>
2019-06-25 22:48:19 -0700

    add threadlist

--- vs-forum/src/components/HelloWorld.vue
+++ vs-forum/src/components/HelloWorld.vue
@@ -1,34 +1,22 @@
 <template>
-  <div>
-    <div class="col-large push-top" v-for="thread in threads">
-      <h1>{{thread.title}}</h1>
-      <div class="post-list">
-        <div class="post" v-for="postId in thread.posts">
-          <div class="user-info">
-            <a href="#">{{users[posts[postId].userId].name}}</a>
-            <a href="#">
-              <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt>
-            </a>
-            <p class="desktop-only text-small">104 posts</p>
-          </div>
-          <div class="post-content">
-            <p>{{posts[postId].text}}</p>
-          </div>
-          <div class="post-date text-faded">{{posts[postId].publishedAt}}</div>
-        </div>
-      </div>
-    </div>
+  <div class="col-full">
+    <h1>Welcome to the Forum</h1>
+    <ThreadList :threads="threads"/>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
+import ThreadList from './ThreadList'
 
 export default {
   name: 'HelloWorld',
+  components: {
+    ThreadList
+  },
   data () {
     return {
-      threads: sourceData.threads,
+      threads: Object.values(sourceData.threads),
       posts: sourceData.posts,
       users: sourceData.users
     }
